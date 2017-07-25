288c6d43172211521f187e84a61cdaa2d8b571fe
HOME <home@Prashants-MacBook-Pro.local>
2017-07-25 08:02:16 +0545

    new coommir

--- bloodapp/ViewController.swift
+++ bloodapp/ViewController.swift
@@ -8,11 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
+    
+    
+   
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var plusbtn: UIBarButtonItem!
+    
+    
+    var task: [Bloodbank] = []
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredtask = [Bloodbank]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        table.dataSource = self
+        table.delegate = self       //Delegates can be used to define callback methods.and allows method to be passed as parameter.
+        
+        
+        filteredtask = task
+        searchController.searchResultsUpdater = self as UISearchResultsUpdating
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        table.tableHeaderView = searchController.searchBar
+        
+        self.table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        
+
+               // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        // If we haven't typed anything into the search bar then do not filter the results
+        if searchController.searchBar.text! == "" {
+            filteredtask = task
+        } else {
+            // Filter the results
+            filteredtask = task.filter { ($0.name?.lowercased().contains(searchController.searchBar.text!.lowercased()))! }
+            
+            for i in 0..<filteredtask.count  {
+                print(String(describing: String(i)+" "+filteredtask[i].name!));
+            }
+            
+        }
+        
+        self.table.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +65,91 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        //get the data from coredata
+        getdata()
+        
+        
+        //reload the table view
+        table.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredtask.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        
+        let newtask = filteredtask[indexPath.row]
+        
+        
+            cell.textLabel?.text = String(describing:indexPath.row + 1) + "  " + newtask.name! + "  " + newtask.blood! + "  " + String(newtask.contact)
+        
+        return cell
+    }
+    
+    
+    
+    //fetching the data
+    func getdata() {
+        
+        let contex =  (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            task = try contex.fetch(Bloodbank.fetchRequest())
+            filteredtask = task
+        }
+        catch {
+            print("fetching failed")
+        }
+        
+    }
+
+    //delete the item
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let contex =  (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if editingStyle == .delete {
+            let task = filteredtask[indexPath.row]
+            contex.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do{
+                filteredtask = try contex.fetch(Bloodbank.fetchRequest())
+            }
+            catch {
+                print("fetching failed")
+            }
+        }
+        table.reloadData()
+        
+        
+    }
+    
+    //perform segue
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "onesegue", sender: indexPath)
+    }
+    
+//transfer data from one segue to another (prepare for segue)
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "onesegue" {         //onesegue is identifier
+            if let toViewController = segue.destination as? addViewController
+            {
+                
+                if let indexPath = sender as? IndexPath {
+                     toViewController.bloodbank = filteredtask[indexPath.row]
+                }
+               
+            }
+        }
+        
+    }
+}
+class mytableviewcell {
 
 }
-
--- bloodapp/addViewController.swift
+++ bloodapp/addViewController.swift
@@ -8,28 +8,149 @@
 
 import UIKit
 
-class addViewController: UIViewController {
+class addViewController: UIViewController, UIPickerViewDataSource , UIPickerViewDelegate {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var namefield: UITextField!   //A weak reference is just a pointer to an object that doesn't protect the object                                                             from being deallocated by ARC.
+    
+    @IBOutlet weak var contactfield: UITextField!
+    
+    @IBOutlet weak var bloodpicker: UIPickerView!
+    
+    var bloodbank: Bloodbank!
+   
+    
+    let blood = ["A+" , "A-" , "B+" , "B-" , "AB+" , "AB-" , "o+" , "o-"] //creating array of blood type
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {   //picker view ko 1 ota matai basxa 2 halo vani 2ta awxa
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { //blood vanni array ko row ayera basxa
+        return blood[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { //count garxa no. of items
+        return blood.count
+    }
+    
+    /*
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        bloodlabel.text = blood[row]
+    }
+    */
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        self.namefield.delegate = self as? UITextFieldDelegate
+        self.contactfield.delegate = self as? UITextFieldDelegate
+        
+        if bloodbank != nil{    //print the value in bloodbank entity if not nil
+            self.namefield.text = bloodbank.name
+            self.contactfield.text = String(bloodbank.contact)
+            
+            //print value for pickerview
+            let postionOfBloodInArray = blood.index(of: bloodbank.blood!)
+            if(postionOfBloodInArray != nil){
+                self.bloodpicker.selectRow(postionOfBloodInArray!, inComponent: 0, animated: false)//(inComponent: postionOfBloodInArray!)
+            }
+            
+            //print(bloodbank)
+        }
+        
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func addbtn(_ sender: Any) {  //adding action to button
+        //guard self.namefield.text != ""  else {return}
+        
+        //alert if the field is empty
+        if((namefield.text?.characters.count)! == 0 || (contactfield.text?.characters.count)! == 0 )
+        {
+            let alertController = UIAlertController(title: "text field is empty", message:"please fill the credential", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            
+        }
+            //alert if contact is less than 10digit number
+            else if ((contactfield.text?.characters.count)! < 10 )
+        {
+            let alert = UIAlertController(title: "number must be 10 digits", message: "please fill the credential", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Ignore and Add", style: UIAlertActionStyle.default, handler: ignorehandler))
+            
+            self.present(alert, animated: true, completion: nil)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+            
+            }
+            
+        else {
+            
+        addtobutton()
+            
+        }
     }
-    */
 
+    //This is for the keyboard to GO AWAYY !! when user clicks anywhere on the view
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //This is for the keyboard to GO AWAYY !! when user clicks "Return" key  on the keyboard
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return false to ignore.
+    {
+        namefield.resignFirstResponder()
+        return true
+    }
+    
+    //function for alert while pressing cancel
+    
+    func ignorehandler(alert: UIAlertAction)  {
+       addtobutton()
+       
+    }
+    
+    //saving the value in entity
+    func addtobutton()
+        {
+            if(bloodbank != nil){
+                
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                
+                bloodbank.name = namefield.text     //namefield ko data bloodbank entity ko name attribute ma save garcha.
+                bloodbank.blood = blood[bloodpicker.selectedRow(inComponent: 0)]
+                bloodbank.contact = Int64(contactfield.text!)!
+                
+                appDelegate.saveContext()
+                
+            }
+                
+            else
+            {
+                
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let task = Bloodbank(context: context)
+            task.name = namefield.text
+            task.contact = Int64(contactfield.text!)!
+            task.blood = blood[bloodpicker.selectedRow(inComponent: 0)]
+            //save the data
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext() //contex save garcha
+            }
+            
+            navigationController!.popViewController(animated: true) //viewcontroller popup garcha
+   
+            }
+    
+    
+    
 }
